4dffdab6d7de20406a431b2a8e039db4b1597a00
Raphael Hetherington <raph37heth@gmail.com>
2021-02-01 09:06:32 +0000

    esbuild wasm working in public dire

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
+import * as esbuild from 'esbuild-wasm';
 
 function App() {
-  return <h1>Hi</h1>;
+  const ref = useRef<any>();
+  //input
+  const [input, setInput] = useState('');
+  // output
+  const [code, setCode] = useState('');
+
+  useEffect(() => {
+    startService();
+  }, []);
+  //handler
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015',
+    });
+
+    setCode(result.code);
+  };
+
+  const startService = async () => {
+    // the initialization
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+
+  return (
+    <div>
+      <textarea
+        name=""
+        id=""
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
